9655a1777188648504cc800545ed1b568a977f04
Knu09 <christiandelatorre1985@gmail.com>
2025-03-04 18:01:42 +0800

    update version

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,78 @@
+<<<<<<< HEAD
 import { Text, View } from "react-native";
 import "../global.css"
+=======
+import { Route } from "expo-router/build/Route";
+import {
+    StyleSheet,
+    View,
+    ScrollView,
+    Button,
+    SafeAreaView,
+    Text,
+} from "react-native";
+import { Stack, useRouter } from "expo-router";
+import MaskedView from "@react-native-masked-view/masked-view";
+import { LinearGradient } from "expo-linear-gradient";
+
+// import { COLORS, icons, images, SIZES } from '../contants';
+>>>>>>> 25db753 (update home page and reinstall android)
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Hello World! Edit app/index.tsx to edit this screen.</Text>
-            <Text> WOrl!</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View
+                style={{
+                    flex: 1,
+                }}
+            >
+                {/* <MaskedView */}
+                {/*     maskElement={<Text style={styles.title}>SleepSpec.</Text>} */}
+                {/* > */}
+                {/*     <LinearGradient */}
+                {/*         colors={["#006EFF", "#7800D3"]} */}
+                {/*         style={styles.gradient} */}
+                {/*     /> */}
+                {/* </MaskedView> */}
+                <Text style={styles.title}>SleepSpec.</Text>
+                <Text style={styles.subtitle}>
+                    Sleep Deprivation Detection using SVM
+                </Text>
+            </View>
+            <View>
+                <Text>Start </Text>
+                {/* <Button> */}
+                {/*     <View> */}
+                {/*         <Text>Select language</Text> */}
+                {/*         <Text> */}
+                {/*         </Text> */}
+                {/*     </View> */}
+                {/* </Button> */}
+            </View>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#01000F",
+        alignItems: "center",
+    },
+
+    title: {
+        fontSize: 48,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center",
+    },
+
+    subtitle: {
+        fontSize: 38,
+        fontWeight: "bold",
+        color: "white",
+        textAlign: "center",
+    },
+
+    gradient: { flex: 1 },
+});
