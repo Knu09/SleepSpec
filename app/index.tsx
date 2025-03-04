70e231a4d310b2be9633574c1f2cde5519b6dd8b
Knu09 <christiandelatorre1985@gmail.com>
2025-03-04 23:41:33 +0800

    Merge conflict index

--- app/index.tsx
+++ app/index.tsx
@@ -10,17 +10,17 @@ import {
     Image,
     Text,
 } from "react-native";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { Stack, useRouter } from "expo-router";
+import { Stack, useRouter, Link } from "expo-router";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { SafeAreaView } from "react-native-safe-area-context";
 // import MaskedView from "@react-native-masked-view/masked-view";
 // import { LinearGradient } from "expo-linear-gradient";
-
-// import { COLORS, icons, images, SIZES } from '../contants';
 
 export default function Index() {
     return (
         <SafeAreaView style={styles.container}>
             <View
+                className="mt-10 px-3"
                 style={{
                     flex: 1,
                 }}
@@ -34,25 +34,58 @@ export default function Index() {
                 {/*     /> */}
                 {/* </MaskedView> */}
                 <View>
-                    <Text style={[styles.title, styles.textalignCenter]}>
+                    <Text className="text-center text-5xl text-white font-bold">
                         SleepSpec.
                     </Text>
-                    <Text style={styles.subtitle}>
-                        Sleep Deprivation Detection using SVM
+                    <Text className="text-center mt-2 text-xl text-white font-bold">
+                        Sleep Deprition Detection using SVM
                     </Text>
                 </View>
-                <View
-                    style={{ justifyContent: "center", alignItems: "center" }}
-                >
-                    <Pressable onPress={() => console.log("Pressed")}>
-                        <Text style={styles.text}>Select language</Text>
-                        <View style={styles.row}>
-                            <Image
-                                source={require("../assets/images/philippines 1.png")}
-                            />
-                            <Text style={styles.text}>Filipino</Text>
-                        </View>
-                    </Pressable>
+                <View className="my-12 items-center gap-4">
+                    <View>
+                        <Text className="text-white text-center">
+                            Start
+                            <View className="inline-flex flex-row items-center mx-2 my-3 py-2 px-2 bg-[#35007680] rounded-full">
+                                <Icon
+                                    className="me-2"
+                                    name="microphone"
+                                    size={20}
+                                    color={"#006FFF"}
+                                />
+                                <Text className="text-primary font-semibold text-[#006FFF]">
+                                    detecting
+                                </Text>
+                            </View>
+                            sleep deprivation by clicking the microphone below.
+                        </Text>
+                    </View>
+                    <View>
+                        <Pressable
+                            className="rounded-3xl items-center py-3 px-10 border border-slate-50"
+                            style={styles.button}
+                            onPress={() => console.log("Pressed")}
+                        >
+                            <Text className="font-bold text-white mb-2">
+                                Select language
+                            </Text>
+                            <View className="flex flex-row text gap-2">
+                                <Image
+                                    source={require("../assets/images/philippines 1.png")}
+                                />
+                                <Text className="text-white">Filipino</Text>
+                            </View>
+                        </Pressable>
+                    </View>
+                </View>
+                <View className="flex items-center t-10 ">
+                    <Link href={"/"}>
+                        <Icon
+                            className="me-2"
+                            name="microphone"
+                            size={20}
+                            color={"#FFF"}
+                        />
+                    </Link>
                 </View>
             </View>
         </SafeAreaView>
@@ -60,30 +93,12 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+    button: {},
+
     container: {
         flex: 1,
         backgroundColor: "#01000F",
         alignItems: "center",
-        color: "white",
-        textAlign: "center",
-    },
-
-    textalignCenter: {
-        textAlign: "center",
-    },
-
-    text: {
-        color: "white",
-    },
-    title: {
-        fontSize: 48,
-        fontWeight: "bold",
-        color: "white",
-    },
-
-    subtitle: {
-        fontSize: 38,
-        fontWeight: "bold",
         color: "white",
         textAlign: "center",
     },
