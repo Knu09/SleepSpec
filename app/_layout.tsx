803458c289787584dfe9fbe2353b855423dd1ecd
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-09 09:55:00 +0800

    add recording to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native";
+
+const { Screen } = Stack;
+const accent = "#006fff";
 
 export default function RootLayout() {
-    return <Stack screenOptions={{ headerShown: false }} />;
+    return (
+        <Stack
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "#01000f",
+                },
+                headerTitleStyle: {
+                    color: "#ddd",
+                    fontWeight: 700,
+                },
+                headerTintColor: accent,
+                headerRight: () => <Text className="text-white">Menu</Text>,
+                headerTitleAlign: "center",
+            }}
+        >
+            <Screen name="index" options={{ title: "Home", headerShown: false }} />
+            <Screen name="recording" options={{ title: "Recording" }} />
+        </Stack>
+    );
 }
