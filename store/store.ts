7ef912ce7400f6042b1aed57f03f7e4daa5b522b
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 13:28:40 +0800

    annotate types

--- store/store.ts
+++ store/store.ts
@@ -1,7 +1,12 @@
 import { create } from 'zustand';
-import type Lang from '@/constants/speech_scripts'
+import Lang from '@/constants/speech_scripts'
 
-export const useLangStore = create((set) => ({
-    currentLang: undefined,
+type LangStore = {
+    currentLang: Lang;
+    setCurrentLang: (language: Lang) => void;
+}
+
+export const useLangStore = create<LangStore>((set) => ({
+    currentLang: Lang.LOREM,
     setCurrentLang: (language: Lang) => set({ currentLang: language }),
 }));
