2cdb53305477eb66fa58a0c75780e42b794e04d5
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-03-10 17:40:26 +0800

    add custom record option

---
+++ constants/rc_option.ts
@@ -0,0 +1,39 @@
+import {
+    RecordingOptions,
+    AndroidOutputFormat,
+    AndroidAudioEncoder,
+    IOSOutputFormat,
+    IOSAudioQuality,
+} from "expo-av/build/Audio";
+
+const MONO = 1;
+const BIT_RATE = 128000;
+
+const CustomRCPreset: RecordingOptions = {
+    isMeteringEnabled: true,
+    android: {
+        extension: ".m4a",
+        outputFormat: AndroidOutputFormat.MPEG_4,
+        audioEncoder: AndroidAudioEncoder.AAC,
+        sampleRate: 44100,
+        numberOfChannels: MONO,
+        bitRate: BIT_RATE,
+    },
+    ios: {
+        extension: ".m4a",
+        outputFormat: IOSOutputFormat.MPEG4AAC,
+        audioQuality: IOSAudioQuality.MAX,
+        sampleRate: 44100,
+        numberOfChannels: MONO,
+        bitRate: BIT_RATE,
+        linearPCMBitDepth: 16,
+        linearPCMIsBigEndian: false,
+        linearPCMIsFloat: false,
+    },
+    web: {
+        mimeType: "audio/webm",
+        bitsPerSecond: BIT_RATE,
+    },
+};
+
+export default CustomRCPreset;
