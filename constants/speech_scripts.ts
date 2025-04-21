ee9b901c0414b5865de0bdf969f5fe9fe148d7d7
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-21 10:50:33 +0800

    remove lorem, change keys to LANG items

--- constants/speech_scripts.ts
+++ constants/speech_scripts.ts
@@ -1,11 +1,10 @@
-enum Lang {
-    LOREM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ex sem, cursus vel nisi at, laoreet eleifend urna. Suspendisse aliquam vehicula magna non ullamcorper. Fusce ut tincidunt nunc, sed egestas nisi. Donec placerat est a neque porttitor consequat. Suspendisse lacus mi, condimentum eu sollicitudin at, rutrum quis nisl.\n\n" +
-        "In consectetur eu magna vel placerat. Proin tempor augue turpis, congue eleifend metus porttitor sed. Etiam varius ex ac orci fringilla rutrum. Etiam vel dui quis ante accumsan consequat at vitae metus. Donec vitae orci pretium, pellentesque velit ut, placerat ipsum. Nullam sagittis ligula sem, sit amet lobortis nibh aliquam ut. Cras sed risus rhoncus, pretium nisi ut, eleifend mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas eu nibh at metus viverra dapibus. Mauris bibendum rhoncus elit id sodales. Suspendisse nec odio elementum, cursus ligula vitae, hendrerit nisl. Suspendisse in urna pulvinar, interdum orci vitae, hendrerit nunc. Etiam eu est malesuada, sodales risus sit amet, ullamcorper nulla.",
+import { LANG } from "@/types/types";
 
-    FILIPINO = `Ngayon, nais kong pag-usapan ang isang napakahalagang paksa na madalas nating binabalewala—ang kahalagahan ng pagtulog. Sa mundong mabilis ang takbo, kadalasan nating inuuna ang tulog, pag-aaral, at iba pang gawain kaysa ang ating kalusugan. Ngunit, alam ba ninyo na ang kawalan ng sapat na tulog ay may masamang epekto sa ating katawan at isipan?
+const SCRIPTS = {
+    [LANG.FILIPINO]: `Ngayon, nais kong pag-usapan ang isang napakahalagang paksa na madalas nating binabalewala—ang kahalagahan ng pagtulog. Sa mundong mabilis ang takbo, kadalasan nating inuuna ang tulog, pag-aaral, at iba pang gawain kaysa ang ating kalusugan. Ngunit, alam ba ninyo na ang kawalan ng sapat na tulog ay may masamang epekto sa ating katawan at isipan?
     Kapag kulang tayo sa tulog, bumababa ang ating
 `,
-    ENGLISH = "He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow’s technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful, daughter-like, loving wife, and three blithe, ruddy children; every Sunday went to a cheerful-looking church, planted in a grove.\n\nBut one night, under cover of darkness, and further concealed in a most cunning disguisement, a desperate burglar slid into his happy home, and robbed them all of everything. And darker yet to tell, the blacksmith himself did ignorantly conduct this burglar into his family’s heart. It was the Bottle Conjuror! Upon the opening of that fatal cork, forth flew the fiend, and shriveled up his home.",
+    [LANG.ENGLISH]: "He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow’s technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful, daughter-like, loving wife, and three blithe, ruddy children; every Sunday went to a cheerful-looking church, planted in a grove.\n\nBut one night, under cover of darkness, and further concealed in a most cunning disguisement, a desperate burglar slid into his happy home, and robbed them all of everything. And darker yet to tell, the blacksmith himself did ignorantly conduct this burglar into his family’s heart. It was the Bottle Conjuror! Upon the opening of that fatal cork, forth flew the fiend, and shriveled up his home.",
     // ENGLISH = `
     //     He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow’s technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful,
     //     daughter-like, loving wife, and three blithe, ruddy children; every Sunday went
@@ -27,4 +26,4 @@ enum Lang {
     // `,
 }
 
-export default Lang;
+export default SCRIPTS;
