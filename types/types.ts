49e7485f05a2bb855935fe86f31ba95595edec74
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-21 14:17:13 +0800

    implement initial working version for selecting language and script based on the selected language

--- types/types.ts
+++ types/types.ts
@@ -1,4 +1,24 @@
 export enum LANG {
     ENGLISH,
-    FILIPINO
+    FILIPINO,
+}
+
+export namespace LANG {
+    export function asString(lang: LANG): string {
+        switch (lang) {
+            case LANG.ENGLISH:
+                return "English";
+            case LANG.FILIPINO:
+                return "Filipino";
+        }
+    }
+
+    export function asImg(lang: LANG): NodeJS.Require {
+        switch (lang) {
+            case LANG.ENGLISH:
+                return require("@/assets/images/flag-us.svg");
+            case LANG.FILIPINO:
+                return require("@/assets/images/flag-ph.svg");
+        }
+    }
 }
