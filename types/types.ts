9eb559d851d62441a6fca3eacf5e621f4b9f1106
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-27 23:58:54 +0800

    add CLASS type

--- types/types.ts
+++ types/types.ts
@@ -13,12 +13,28 @@ export namespace LANG {
         }
     }
 
-    export function asImg(lang: LANG): NodeJS.Require {
-        switch (lang) {
+    export function asImg(self: LANG): NodeJS.Require {
+        switch (self) {
             case LANG.ENGLISH:
                 return require("@/assets/images/flag-us.svg");
             case LANG.FILIPINO:
                 return require("@/assets/images/flag-ph.svg");
+        }
+    }
+}
+
+export enum CLASS {
+    SD, // Sleep deprived
+    NSD, // Not Sleep deprived
+}
+
+export namespace CLASS {
+    export function toHeader(self: CLASS): string {
+        switch (self) {
+            case CLASS.SD:
+                return "Highly Sleep-Deprived"
+            case CLASS.NSD:
+                return "Not Sleep-Deprived"
         }
     }
 }
