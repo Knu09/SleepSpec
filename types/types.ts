2606a3d8c9f745c73b8c7c9cb264c97e14b70ce7
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-28 09:31:56 +0800

    add class and class_result enums

--- types/types.ts
+++ types/types.ts
@@ -21,20 +21,74 @@ export namespace LANG {
                 return require("@/assets/images/flag-ph.svg");
         }
     }
+
+    export function getScript(self: LANG): string {
+        return scripts[self]
+    }
 }
 
 export enum CLASS {
-    SD, // Sleep deprived
     NSD, // Not Sleep deprived
+    SD, // Sleep deprived
 }
 
-export namespace CLASS {
-    export function toHeader(self: CLASS): string {
-        switch (self) {
+export type ClassResult = {
+    class: CLASS,
+    confidence_score: number,
+}
+
+export namespace ClassResult {
+    type ResultObj = {
+        class: number,
+        confidence_score: number,
+    }
+
+    export function toHeader(self: ClassResult): string {
+        switch (self.class) {
             case CLASS.SD:
                 return "Highly Sleep-Deprived"
             case CLASS.NSD:
                 return "Not Sleep-Deprived"
         }
     }
+
+    export function fromJSON(data: string): ClassResult {
+        const { class: c, confidence_score }: ResultObj = JSON.parse(data)
+        const result: ClassResult = {
+            class: c == 1 ? CLASS.SD : CLASS.NSD,
+            confidence_score
+        }
+
+        return result
+    }
 }
+
+
+const scripts = {
+    [LANG.ENGLISH]:
+        "He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow’s technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful, daughter-like, loving wife, and three blithe, ruddy children; every Sunday went to a cheerful-looking church, planted in a grove.\n\nBut one night, under cover of darkness, and further concealed in a most cunning disguisement, a desperate burglar slid into his happy home, and robbed them all of everything. And darker yet to tell, the blacksmith himself did ignorantly conduct this burglar into his family’s heart. It was the Bottle Conjuror! Upon the opening of that fatal cork, forth flew the fiend, and shriveled up his home.",
+
+    [LANG.FILIPINO]:
+        `Ngayon, nais kong pag-usapan ang isang napakahalagang paksa na madalas nating binabalewala—ang kahalagahan ng pagtulog. Sa mundong mabilis ang takbo, kadalasan nating inuuna ang tulog, pag-aaral, at iba pang gawain kaysa ang ating kalusugan. Ngunit, alam ba ninyo na ang kawalan ng sapat na tulog ay may masamang epekto sa ating katawan at isipan?
+    Kapag kulang tayo sa tulog, bumababa ang ating
+`,
+    // ENGLISH = `
+    //     He was an old man, who, at the age of nearly sixty, had postponedly encountered that thing in sorrow’s technicals called ruin. He had been an artisan of famed excellence, and with plenty to do; owned a house and garden; embraced a youthful,
+    //     daughter-like, loving wife, and three blithe, ruddy children; every Sunday went
+    //     to a cheerful-looking church, planted in a grove.
+    //
+    //     But one night, under cover of darkness, and further concealed in a most cunning
+    //     disguisement, a desperate burglar slid into his happy home, and robbed them all
+    //     of everything. And darker yet to tell, the blacksmith himself did ignorantly
+    //     conduct this burglar into his family’s heart. It was the Bottle Conjuror! Upon
+    //     the opening of that fatal cork, forth flew the fiend, and shriveled up his home.
+    //
+    //     Now, for prudent, most wise, and economic reasons, the blacksmith’s shop was in
+    //     the basement of his dwelling, but with a separate entrance to it; so that always
+    //     had the young and loving healthy wife listened with no unhappy nervousness, but
+    //     with vigorous pleasure, to the stout ringing of her young-armed old husband’s
+    //     hammer; whose reverberations, muffled by passing through the floors and walls,
+    //     came up to her, not unsweetly, in her nursery; and so, to stout Labor’s iron lullaby,
+    //     the blacksmith’s infants were rocked to slumber.
+    // `,
+};
