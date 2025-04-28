82b53dda7fb317fb941f9d71d9a103568522bbf1
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-04-28 16:08:45 +0800

    add ClassStore

--- store/store.ts
+++ store/store.ts
@@ -1,12 +1,21 @@
 import { create } from "zustand";
-import { LANG } from "@/types/types";
+import { ClassResult, LANG } from "@/types/types";
 
 type LangStore = {
     currentLang: LANG;
     setCurrentLang: (language: LANG) => void;
 };
 
+type ClassStore = {
+    result?: ClassResult;
+    setResult: (cr: ClassResult) => void;
+}
+
 export const useLangStore = create<LangStore>((set) => ({
     currentLang: LANG.ENGLISH,
     setCurrentLang: (language: LANG) => set({ currentLang: language }),
 }));
+
+export const useClassStore = create<ClassStore>((set) => ({
+    setResult: (cr: ClassResult) => set({ result: cr })
+}))
