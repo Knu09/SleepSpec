3d69a7828421a5f6194c7be7701f00e3f2c30d1f
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-05-05 16:20:57 +0800

    copy from analysis

--- app/feature-analysis.tsx
+++ app/feature-analysis.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
 import { SafeAreaView, ScrollView, View, Text } from "react-native";
 import Header from "@/components/Header";
+import { useFonts } from "expo-font";
+import { useClassStore } from "@/store/store";
+import { useRouter, SplashScreen } from "expo-router";
 
 export default function() {
+    const { result } = useClassStore();
+    const router = useRouter();
+
+    const [fontsLoaded] = useFonts({
+        "Poppins-Regular": require(
+            "../assets/fonts/Poppins/Poppins-Regular.ttf",
+        ),
+        "Poppins-Bold": require("../assets/fonts/Poppins/Poppins-Bold.ttf"),
+        "PublicSans-Regular": require(
+            "../assets/fonts/Public_Sans/static/PublicSans-Regular.ttf",
+        ),
+        "PublicSans-Bold": require(
+            "../assets/fonts/Public_Sans/static/PublicSans-Bold.ttf",
+        ),
+    });
+
+    useEffect(() => {
+        if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded]);
+    if (!result) {
+        console.error("No Results Found!");
+        router.back();
+        return
+    }
     return (
         <SafeAreaView className="flex-1 bg-darkBg">
             <Header title={"Feature Analysis"} back={true} menu={true} />
