568bd535634470688fdde04673fa23134c80a8e8
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-05-05 20:37:58 +0800

    render actual proper header based on result

--- app/feature-analysis.tsx
+++ app/feature-analysis.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import { useFonts } from "expo-font";
 import { useClassStore } from "@/store/store";
 import { useRouter, SplashScreen } from "expo-router";
+import { CLASS } from "@/types/types";
 
 export default function() {
     const { result } = useClassStore();
@@ -25,11 +26,15 @@ export default function() {
     useEffect(() => {
         if (fontsLoaded) SplashScreen.hideAsync();
     }, [fontsLoaded]);
+
     if (!result) {
         console.error("No Results Found!");
         router.back();
         return
     }
+
+    const headerColor = result.class == CLASS.SD ? '#ff2121' : '#006fff';
+
     return (
         <SafeAreaView className="flex-1 bg-darkBg">
             <Header title={"Feature Analysis"} back={true} menu={true} />
@@ -44,8 +49,8 @@ export default function() {
             >
                 <View className="flex justify-center items-center text-center text-secondary">
                     <Text className="text-secondary">You are</Text>
-                    <Text className="text-danger font-publicsans text-2xl font-bold">
-                        Highly Sleep-deprived!
+                    <Text style={{ color: headerColor }} className={`font-publicsans text-2xl font-bold`}>
+                        {CLASS.toHeader(result)}
                     </Text>
                 </View>
             </ScrollView>
