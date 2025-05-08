bf51e4663fee3f5f0b6f3a2bce55fdd56c5f3853
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-05-08 16:18:06 +0800

    migrate to SDK 53 replacing expo-av with expo-audio

--- constants/rc_option.ts
+++ constants/rc_option.ts
@@ -1,31 +1,22 @@
-import {
-    AndroidAudioEncoder,
-    AndroidOutputFormat,
-    IOSAudioQuality,
-    IOSOutputFormat,
-    RecordingOptions,
-} from "expo-av/build/Audio";
+import { AudioQuality, IOSOutputFormat, RecordingOptions } from "expo-audio";
 
 const MONO = 1;
 const BIT_RATE = 128000;
 
 const CustomRCPreset: RecordingOptions = {
     isMeteringEnabled: true,
+    numberOfChannels: MONO,
+    bitRate: BIT_RATE,
+    extension: ".m4a",
+    sampleRate: 44100,
+
     android: {
-        extension: ".m4a",
-        outputFormat: AndroidOutputFormat.MPEG_4,
-        audioEncoder: AndroidAudioEncoder.AAC,
-        sampleRate: 44100,
-        numberOfChannels: MONO,
-        bitRate: BIT_RATE,
+        outputFormat: "mpeg4",
+        audioEncoder: "aac",
     },
     ios: {
-        extension: ".m4a",
         outputFormat: IOSOutputFormat.MPEG4AAC,
-        audioQuality: IOSAudioQuality.MAX,
-        sampleRate: 44100,
-        numberOfChannels: MONO,
-        bitRate: BIT_RATE,
+        audioQuality: AudioQuality.MAX,
         linearPCMBitDepth: 16,
         linearPCMIsBigEndian: false,
         linearPCMIsFloat: false,
