3f2c81f3743b514a8bb291512f1ab5bf7c187f2b
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-09 19:37:24 +0800

    Add: imageMap.ts for storing local images

---
+++ store/imageMap.ts
@@ -0,0 +1,5 @@
+const localImages: Record<string, any> = {
+    // "manual.png": require("../assets/images/manual.png"),
+    // "guide.jpg": require("../assets/images/guide.jpg"),
+    // Add all local images you use
+};
