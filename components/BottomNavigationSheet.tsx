fd7b1f33e3b7e90f8dfec13e2602903c11bf4d3a
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-10 12:58:04 +0800

    Fix: Issue in GestureHandlerRootView
    Update: Routes of Links

--- components/BottomNavigationSheet.tsx
+++ components/BottomNavigationSheet.tsx
@@ -16,89 +16,91 @@ import { useRouter } from "expo-router";
 // }
 
 export default function BottomNavigationSheet() {
-  const { isVisible, hideBottomSheet } = useBottomSheet();
-  const bottomSheetRef = useRef<BottomSheet>(null);
-  const router = useRouter();
+    const { isVisible, hideBottomSheet } = useBottomSheet();
+    const bottomSheetRef = useRef<BottomSheet>(null);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (isVisible) {
-      bottomSheetRef.current?.expand();
-    } else {
-      bottomSheetRef.current?.close();
-    }
-  }, [isVisible]);
+    useEffect(() => {
+        if (isVisible) {
+            bottomSheetRef.current?.expand();
+        } else {
+            bottomSheetRef.current?.close();
+        }
+    }, [isVisible]);
 
-  const handleSheetChanges = useCallback(
-    (index: number) => {
-      console.log(index);
-      if (index === -1) {
+    const handleSheetChanges = useCallback(
+        (index: number) => {
+            console.log(index);
+            if (index === -1) {
+                hideBottomSheet();
+            }
+        },
+        [hideBottomSheet],
+    );
+
+    const navigateTo = (screen: string) => {
         hideBottomSheet();
-      }
-    },
-    [hideBottomSheet],
-  );
+        router.push(`/${screen}` as any);
+        // navigation.navigate(screen as never);
+    };
 
-  const navigateTo = (screen: string) => {
-    hideBottomSheet();
-    router.push(`/${screen}` as any);
-    // navigation.navigate(screen as never);
-  };
+    if (!isVisible) return null;
 
-  if (!isVisible) return null;
-
-  return (
-    <GestureHandlerRootView style={StyleSheet.absoluteFill}>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={0}
-        onChange={handleSheetChanges}
-        snapPoints={["30%"]}
-        handleComponent={() => null}
-        enablePanDownToClose={true}
-        backgroundStyle={{ backgroundColor: "#000000" }}
-      >
-        {/* TODO: change the navigation links */}
-        {/* FIXME: blue view not working */}
-        {/* <BlurView intensity={50} tint="dark" style={StyleSheet.absoluteFill} /> */}
-        <BottomSheetView style={styles.bottomsheetContent}>
-          <TouchableOpacity
-            onPress={() => {
-              // hideBottomSheet();
-              // router.push("/");
-              navigateTo("");
-            }}
-          >
-            <Text style={styles.bottomsheetText}>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("feature-analysis")}>
-            <Text style={styles.bottomsheetText}>User Manual</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("feature-analysis")}>
-            <Text style={styles.bottomsheetText}>Trained Model Source</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("feature-analysis")}>
-            <Text style={styles.bottomsheetText}>Training Results</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("feature-analysis")}>
-            <Text style={styles.bottomsheetText}>About Us</Text>
-          </TouchableOpacity>
-        </BottomSheetView>
-      </BottomSheet>
-    </GestureHandlerRootView>
-  );
+    return (
+        <BottomSheet
+            ref={bottomSheetRef}
+            index={0}
+            onChange={handleSheetChanges}
+            snapPoints={["30%"]}
+            handleComponent={() => null}
+            enablePanDownToClose={true}
+            backgroundStyle={{ backgroundColor: "#000000" }}
+        >
+            {/* TODO: change the navigation links */}
+            {/* FIXME: blue view not working */}
+            {/* <BlurView intensity={50} tint="dark" style={StyleSheet.absoluteFill} /> */}
+            <BottomSheetView style={styles.bottomsheetContent}>
+                <TouchableOpacity
+                    onPress={() => {
+                        // hideBottomSheet();
+                        // router.push("/");
+                        navigateTo("");
+                    }}
+                >
+                    <Text style={styles.bottomsheetText}>Home</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigateTo("user_manual")}>
+                    <Text style={styles.bottomsheetText}>User Manual</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigateTo("")}>
+                    <Text style={styles.bottomsheetText}>
+                        Trained Model Source
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => navigateTo("training_results")}
+                >
+                    <Text style={styles.bottomsheetText}>Training Results</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigateTo("about_us")}>
+                    <Text style={styles.bottomsheetText}>About Us</Text>
+                </TouchableOpacity>
+            </BottomSheetView>
+        </BottomSheet>
+    );
 }
 
 const styles = StyleSheet.create({
-  bottomsheetContent: {
-    backgroundColor: "transparent",
-    alignItems: "center",
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    paddingVertical: 20,
-  },
-  bottomsheetText: {
-    color: "#ffffff",
-    marginVertical: 12,
-    fontSize: 16,
-  },
+    bottomsheetContent: {
+        backgroundColor: "transparent",
+        alignItems: "center",
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        paddingVertical: 20,
+    },
+    bottomsheetText: {
+        color: "#ffffff",
+        marginVertical: 12,
+        fontSize: 16,
+    },
 });
