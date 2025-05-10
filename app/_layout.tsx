954abd93472d9babe59c004ccbe9f59963eec9d7
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-10 12:58:32 +0800

    Add: GestureHandlerRootView as the main element Root

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,38 @@
 import { Stack } from "expo-router";
 import BottomNavigationSheet from "@/components/BottomNavigationSheet";
 import { BottomSheetProvider } from "@/components/BottomSheetContext";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const { Screen } = Stack;
 const accent = "#006fff";
 
 export default function RootLayout() {
-  return (
-    <BottomSheetProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Screen name="index" options={{ title: "Home" }} />
-        <Screen name="recording" options={{ title: "Recording" }} />
-        <Screen name="select_language" options={{ title: "Select Language" }} />
-        <Screen name="analysis" options={{ title: "Analysis" }} />
-        <Screen name="classification" options={{ title: "Classification" }} />
-        <Screen
-          name="feature-analysis"
-          options={{ title: "Feature Analysis" }}
-        />
-      </Stack>
-      <BottomNavigationSheet />
-    </BottomSheetProvider>
-  );
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetProvider>
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Screen name="index" options={{ title: "Home" }} />
+                    <Screen name="recording" options={{ title: "Recording" }} />
+                    <Screen
+                        name="select_language"
+                        options={{ title: "Select Language" }}
+                    />
+                    <Screen name="analysis" options={{ title: "Analysis" }} />
+                    <Screen
+                        name="classification"
+                        options={{ title: "Classification" }}
+                    />
+                    <Screen
+                        name="feature-analysis"
+                        options={{ title: "Feature Analysis" }}
+                    />
+                </Stack>
+                <BottomNavigationSheet />
+            </BottomSheetProvider>
+        </GestureHandlerRootView>
+    );
 }
