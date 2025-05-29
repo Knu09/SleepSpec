96cacd4fac3a477a3ea39179449e13728aef4660
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-29 15:40:02 +0800

    Add: Link of Recent Results

--- components/BottomNavigationSheet.tsx
+++ components/BottomNavigationSheet.tsx
@@ -1,15 +1,16 @@
 import { useEffect, useRef, useCallback } from "react";
 import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  TouchableWithoutFeedback,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    TouchableWithoutFeedback,
 } from "react-native";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useBottomSheet } from "./BottomSheetContext";
 import { useNavigation, NavigationProp } from "@react-navigation/native";
 import { useRouter } from "expo-router";
+import { useClassStore } from "@/store/store";
 // import { BlurView } from "expo-blur";
 
 // function RootStack() {
@@ -21,108 +22,129 @@ import { useRouter } from "expo-router";
 // }
 
 export default function BottomNavigationSheet() {
-  const { isVisible, hideBottomSheet } = useBottomSheet();
-  const bottomSheetRef = useRef<BottomSheet>(null);
-  const router = useRouter();
+    const { isVisible, hideBottomSheet } = useBottomSheet();
+    const bottomSheetRef = useRef<BottomSheet>(null);
+    const router = useRouter();
 
-  useEffect(() => {
-    if (isVisible) {
-      bottomSheetRef.current?.expand();
-    } else {
-      bottomSheetRef.current?.close();
-    }
-  }, [isVisible]);
+    useEffect(() => {
+        if (isVisible) {
+            bottomSheetRef.current?.expand();
+        } else {
+            bottomSheetRef.current?.close();
+        }
+    }, [isVisible]);
 
-  const handleSheetChanges = useCallback(
-    (index: number) => {
-      console.log(index);
-      if (index === -1) {
+    const handleSheetChanges = useCallback(
+        (index: number) => {
+            console.log(index);
+            if (index === -1) {
+                hideBottomSheet();
+            }
+        },
+        [hideBottomSheet],
+    );
+
+    const navigateTo = (screen: string) => {
         hideBottomSheet();
-      }
-    },
-    [hideBottomSheet],
-  );
+        router.push(`/${screen}` as any);
+        // navigation.navigate(screen as never);
+    };
 
-  const navigateTo = (screen: string) => {
-    hideBottomSheet();
-    router.push(`/${screen}` as any);
-    // navigation.navigate(screen as never);
-  };
+    const { result, setResult } = useClassStore();
 
-  if (!isVisible) return null;
+    if (!isVisible) return null;
 
-  return (
-    <>
-      <TouchableWithoutFeedback onPress={hideBottomSheet}>
-        <View style={styles.overlay} />
-      </TouchableWithoutFeedback>
-      <BottomSheet
-        ref={bottomSheetRef}
-        index={0}
-        onChange={handleSheetChanges}
-        snapPoints={["30%"]}
-        handleComponent={() => null}
-        enablePanDownToClose={true}
-        backgroundStyle={{ backgroundColor: "transparent" }}
-        style={styles.bottomSheetWrapper}
-      >
-        {/* TODO: change the navigation links */}
-        {/* FIXME: blue view not working */}
-        {/* <BlurView intensity={50} tint="dark" style={StyleSheet.absoluteFill} /> */}
-        <View className="w-6 h-1 bg-white" style={styles.drawerHolder}></View>
-        <BottomSheetView style={styles.bottomsheetContent}>
-          <TouchableOpacity
-            onPress={() => {
-              // hideBottomSheet();
-              // router.push("/");
-              navigateTo("");
-            }}
-          >
-            <Text style={styles.bottomsheetText}>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("user_manual")}>
-            <Text style={styles.bottomsheetText}>User Manual</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("")}>
-            <Text style={styles.bottomsheetText}>Trained Model Source</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("training_results")}>
-            <Text style={styles.bottomsheetText}>Training Results</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigateTo("about_us")}>
-            <Text style={styles.bottomsheetText}>About Us</Text>
-          </TouchableOpacity>
-        </BottomSheetView>
-      </BottomSheet>
-    </>
-  );
+    return (
+        <>
+            <TouchableWithoutFeedback onPress={hideBottomSheet}>
+                <View style={styles.overlay} />
+            </TouchableWithoutFeedback>
+            <BottomSheet
+                ref={bottomSheetRef}
+                index={0}
+                onChange={handleSheetChanges}
+                snapPoints={["38%"]}
+                handleComponent={() => null}
+                enablePanDownToClose={true}
+                backgroundStyle={{ backgroundColor: "transparent" }}
+                style={styles.bottomSheetWrapper}
+            >
+                {/* TODO: change the navigation links */}
+                {/* FIXME: blue view not working */}
+                {/* <BlurView intensity={50} tint="dark" style={StyleSheet.absoluteFill} /> */}
+                <View
+                    className="w-6 h-1 bg-white"
+                    style={styles.drawerHolder}
+                ></View>
+                <BottomSheetView style={styles.bottomsheetContent}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            // hideBottomSheet();
+                            // router.push("/");
+                            navigateTo("");
+                        }}
+                    >
+                        <Text style={styles.bottomsheetText}>Home</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigateTo("user_manual")}>
+                        <Text style={styles.bottomsheetText}>User Manual</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigateTo("")}>
+                        <Text style={styles.bottomsheetText}>
+                            Trained Model Source
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigateTo("training_results")}
+                    >
+                        <Text style={styles.bottomsheetText}>
+                            Training Results
+                        </Text>
+                    </TouchableOpacity>
+                    {result && (
+                        <TouchableOpacity
+                            onPress={() => navigateTo("analysis")}
+                        >
+                            <Text style={styles.bottomsheetText}>
+                                View Recent Test Results
+                            </Text>
+                        </TouchableOpacity>
+                    )}
+
+                    <TouchableOpacity onPress={() => navigateTo("about_us")}>
+                        <Text style={styles.bottomsheetText}>About Us</Text>
+                    </TouchableOpacity>
+                </BottomSheetView>
+            </BottomSheet>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  bottomSheetWrapper: {
-    backgroundColor: "#23242A",
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-  },
-  bottomsheetContent: {
-    paddingTop: 20,
-    paddingBottom: 30,
-    paddingHorizontal: 20,
-  },
-  bottomsheetText: {
-    color: "#DDDDDD",
-    marginVertical: 12,
-    fontSize: 16,
-  },
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    bottomSheetWrapper: {
+        backgroundColor: "#23242A",
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+    },
+    bottomsheetContent: {
+        paddingTop: 20,
+        paddingBottom: 30,
+        paddingHorizontal: 20,
+    },
+    bottomsheetText: {
+        color: "#DDDDDD",
+        marginVertical: 12,
+        fontSize: 16,
+    },
 
-  drawerHolder: {
-    top: 15,
-    left: "47%",
-    borderRadius: 20,
-    zIndex: 100,
-  },
+    drawerHolder: {
+        top: 15,
+        left: "47%",
+        borderRadius: 20,
+        zIndex: 100,
+    },
 });
