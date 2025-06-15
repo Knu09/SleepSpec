03392fb4eb486ac21a56ad8cbac99883f6bc05f1
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-16 01:48:12 +0800

    Add: General switch settings
    Add: Noise reduction, Background noise reduction, and Dark Mode

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,4 +1,11 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Switch,
+} from "react-native";
 import { Link } from "expo-router";
 import Icon from "@expo/vector-icons/FontAwesome";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -10,6 +17,7 @@ import { useEffect, useRef } from "react";
 SplashScreen.preventAutoHideAsync();
 
 import Header from "@/components/Header";
+import SettingButton from "@/components/SettingButton";
 
 export default function Settings() {
     const [fontsLoaded] = useFonts({
@@ -28,9 +36,50 @@ export default function Settings() {
         <SafeAreaView className="bg-darkBg" style={styles.container}>
             <StatusBar />
             <Header back={true} menu={true} title="Settings" />
-            <View>
-                <Text className="text-secondary">Settings</Text>
-            </View>
+            <ScrollView
+                className="px-6 w-full text-start"
+                contentContainerStyle={{
+                    flexGrow: 1,
+                    alignItems: "start",
+                    gap: 10,
+                    justifyContent: "start",
+                }}
+            >
+                <View className="gap-5">
+                    <View className="gap-2">
+                        <Text className="text-secondary font-publicsans">
+                            General
+                        </Text>
+                        <View className="bg-darkGray p-4 rounded-lg gap-5">
+                            <View className="flex flex-row justify-between items-center">
+                                <Text className="text-secondary">
+                                    Noise Reduction
+                                </Text>
+                                <Switch />
+                            </View>
+                            <View className="flex flex-row justify-between items-center">
+                                <Text className="text-secondary">
+                                    Background Noise Remover
+                                </Text>
+                                <Switch />
+                            </View>
+                            <View className="flex flex-row justify-between items-center">
+                                <Text className="text-secondary">
+                                    Dark Mode
+                                </Text>
+                                <Switch />
+                            </View>
+                        </View>
+                    </View>
+
+                    <View className="gap-2">
+                        <Text className="text-secondary font-publicsans">
+                            UI Mode
+                        </Text>
+                        <SettingButton />
+                    </View>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
