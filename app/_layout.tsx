4499b28e877d35d03a736012155b62db83f6364f
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-18 21:25:52 +0800

    Wrap: ThemeProvider by ThemeContext in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,38 +2,47 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Stack } from "expo-router";
 import BottomNavigationSheet from "@/components/BottomNavigationSheet";
 import { BottomSheetProvider } from "@/components/BottomSheetContext";
+import ThemeProvider from "@/context/ThemeContext";
 
 const { Screen } = Stack;
 const accent = "#006fff";
 
 export default function RootLayout() {
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <BottomSheetProvider>
-                <Stack
-                    screenOptions={{
-                        headerShown: false,
-                        animation: "fade_from_bottom",
-                    }}
-                >
-                    <Screen name="index" options={{ title: "" }} />
-                    <Screen name="recording" options={{ title: "Recording" }} />
-                    <Screen
-                        name="select_language"
-                        options={{ title: "Select Language" }}
-                    />
-                    <Screen name="analysis" options={{ title: "Analysis" }} />
-                    <Screen
-                        name="classification"
-                        options={{ title: "Classification" }}
-                    />
-                    <Screen
-                        name="feature-analysis"
-                        options={{ title: "Feature Analysis" }}
-                    />
-                </Stack>
-                <BottomNavigationSheet />
-            </BottomSheetProvider>
-        </GestureHandlerRootView>
+        <ThemeProvider>
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <BottomSheetProvider>
+                    <Stack
+                        screenOptions={{
+                            headerShown: false,
+                            animation: "fade_from_bottom",
+                        }}
+                    >
+                        <Screen name="index" options={{ title: "" }} />
+                        <Screen
+                            name="recording"
+                            options={{ title: "Recording" }}
+                        />
+                        <Screen
+                            name="select_language"
+                            options={{ title: "Select Language" }}
+                        />
+                        <Screen
+                            name="analysis"
+                            options={{ title: "Analysis" }}
+                        />
+                        <Screen
+                            name="classification"
+                            options={{ title: "Classification" }}
+                        />
+                        <Screen
+                            name="feature-analysis"
+                            options={{ title: "Feature Analysis" }}
+                        />
+                    </Stack>
+                    <BottomNavigationSheet />
+                </BottomSheetProvider>
+            </GestureHandlerRootView>
+        </ThemeProvider>
     );
 }
