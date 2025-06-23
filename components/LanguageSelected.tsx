41541cdf0ae48662e1d32570ca359651863d6123
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-23 22:10:38 +0800

    add feat: dark mode in language selection component

--- components/LanguageSelected.tsx
+++ components/LanguageSelected.tsx
@@ -2,16 +2,23 @@ import { Text, View } from "react-native";
 import { Image } from "expo-image";
 import { LANG } from "@/types/types";
 import { useLangStore } from "@/store/store";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 export default function LanguageSelected() {
     const { currentLang: lang } = useLangStore();
+    const { currentTheme } = useContext(ThemeContext);
+    const isDark = currentTheme === "dark";
+    const textClass = isDark ? "text-secondary" : "text-darkBg";
     return (
         <View className="flex flex-row text pl-[2px]">
             <Image
                 source={LANG.asImg(lang)}
                 style={{ width: 25, aspectRatio: 1, marginRight: 6 }}
             />
-            <Text className="text-lg text-white font-normal">
+            <Text
+                className={textClass + " text-lg font-normal font-publicsans"}
+            >
                 {LANG.asString(lang)}
             </Text>
         </View>
