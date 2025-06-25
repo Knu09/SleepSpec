002333dc5a547ca3e347a1985ea520df7543475c
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-25 22:24:53 +0800

    add feat: light mode in Tab Navigation

--- components/TabNavigation.tsx
+++ components/TabNavigation.tsx
@@ -1,6 +1,8 @@
+import { ThemeContext } from "@/context/ThemeContext";
 import { Image, ImageSource } from "expo-image";
 import { Href, useRouter } from "expo-router";
 import { useRouteInfo } from "expo-router/build/hooks";
+import { useContext } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type TabProps = {
@@ -12,8 +14,11 @@ type TabProps = {
 export default function TabNavigation() {
     return (
         <View
-            className="border border-t-lightWhite/50"
-            style={{ justifyContent: "center" }}
+            style={{
+                justifyContent: "center",
+                borderTopWidth: 0.5,
+                borderTopColor: "#808080",
+            }}
         >
             <View className="flex-row" style={styles.nav}>
                 <Tab
@@ -41,21 +46,28 @@ function Tab({ icon, name, link }: TabProps) {
     const router = useRouter();
     const ICON_SIZE = 30;
 
+    const { currentTheme } = useContext(ThemeContext);
+    const isDark = currentTheme === "dark";
+    const textClass = isDark ? "text-white" : "text-darkBg";
+
     const is_selected = PATH == link;
 
     return (
         <Pressable onPress={() => is_selected || router.replace(link)}>
             <View
                 className="flex items-center gap-1"
-                style={{ opacity: is_selected ? 1 : 0.7 }}
+                style={{ opacity: is_selected ? 1 : 0.5 }}
             >
                 <Image
                     source={icon}
                     style={{ width: ICON_SIZE, height: ICON_SIZE }}
                 />
                 <Text
-                    className="font-medium text-xs text-center"
-                    style={{ color: is_selected ? "#ddd" : "gray" }}
+                    className={
+                        textClass +
+                        " font-publicsans font-bold text-sm text-center"
+                    }
+                    style={{ opacity: is_selected ? 1 : 0.5 }}
                 >
                     {name}
                 </Text>
