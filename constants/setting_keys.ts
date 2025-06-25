befab37e3f6595f1f2ca11b168479f333eba4850
GizakiF <aranetagian4221@gmail.com>
2025-06-25 16:16:30 +0800

    used aync storage to save and load settings from local device

---
+++ constants/setting_keys.ts
@@ -0,0 +1,6 @@
+const SettingKeys = {
+  APP_THEME: "appTheme",
+  USER_SETTINGS: "userSettings",
+};
+
+export default SettingKeys;
--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,26 +1,43 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import SettingKeys from "@/constants/setting_keys";
 
 type ThemeContextType = {
-    currentTheme: string;
-    toggleTheme: (newTheme: string) => void;
+  currentTheme: string;
+  toggleTheme: (newTheme: string) => void;
 };
 export const ThemeContext = createContext<ThemeContextType>({
-    currentTheme: "dark",
-    toggleTheme: () => {},
+  currentTheme: "dark",
+  toggleTheme: () => {},
 });
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
-    const [theme, setTheme] = useState<string>("dark");
+  const [theme, setTheme] = useState<string>("dark");
 
-    const toggleTheme = (newTheme: string) => {
-        setTheme(newTheme);
+  useEffect(() => {
+    const loadTheme = async () => {
+      try {
+        const savedTheme = await AsyncStorage.getItem(SettingKeys.APP_THEME);
+        if (savedTheme) {
+          setTheme(savedTheme);
+        }
+      } catch (error) {}
     };
+    loadTheme();
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{ currentTheme: theme, toggleTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    );
+  const toggleTheme = async (newTheme: string) => {
+    try {
+      await AsyncStorage.setItem(SettingKeys.APP_THEME, newTheme);
+      setTheme(newTheme);
+    } catch (error) {}
+  };
+
+  return (
+    <ThemeContext.Provider value={{ currentTheme: theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 };
 
 export default ThemeProvider;
