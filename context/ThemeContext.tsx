87d2d334028ff1c2780b959b5a7b2f336406012d
Rafael Jacov Medel <medelrafjac@gmail.com>
2025-07-11 16:09:32 +0800

    format

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -3,41 +3,41 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import SettingKeys from "@/constants/setting_keys";
 
 type ThemeContextType = {
-  currentTheme: string;
-  toggleTheme: (newTheme: string) => void;
+    currentTheme: string;
+    toggleTheme: (newTheme: string) => void;
 };
 export const ThemeContext = createContext<ThemeContextType>({
-  currentTheme: "dark",
-  toggleTheme: () => {},
+    currentTheme: "dark",
+    toggleTheme: () => { },
 });
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<string>("dark");
+    const [theme, setTheme] = useState<string>("dark");
 
-  useEffect(() => {
-    const loadTheme = async () => {
-      try {
-        const savedTheme = await AsyncStorage.getItem(SettingKeys.APP_THEME);
-        if (savedTheme) {
-          setTheme(savedTheme);
-        }
-      } catch (error) {}
+    useEffect(() => {
+        const loadTheme = async () => {
+            try {
+                const savedTheme = await AsyncStorage.getItem(SettingKeys.APP_THEME);
+                if (savedTheme) {
+                    setTheme(savedTheme);
+                }
+            } catch (error) { }
+        };
+        loadTheme();
+    }, []);
+
+    const toggleTheme = async (newTheme: string) => {
+        try {
+            await AsyncStorage.setItem(SettingKeys.APP_THEME, newTheme);
+            setTheme(newTheme);
+        } catch (error) { }
     };
-    loadTheme();
-  }, []);
 
-  const toggleTheme = async (newTheme: string) => {
-    try {
-      await AsyncStorage.setItem(SettingKeys.APP_THEME, newTheme);
-      setTheme(newTheme);
-    } catch (error) {}
-  };
-
-  return (
-    <ThemeContext.Provider value={{ currentTheme: theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+    return (
+        <ThemeContext.Provider value={{ currentTheme: theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 };
 
 export default ThemeProvider;
