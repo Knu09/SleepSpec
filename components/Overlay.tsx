4d12080f20e8cb45f5f68d0a3c9059d61caff239
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-10-29 22:16:59 +0800

    feat (Overlay.tsx): Display Elapsed Time

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect } from "react";
 import { Href, useRouter } from "expo-router";
 import { Process } from "@/types/types";
 import { ThemeContext } from "@/context/ThemeContext";
+import { useTimer } from "@/context/TimerContext";
 
 type OverlayProps = {
     heading: string;
@@ -20,6 +21,7 @@ export default function Overlay({
     const router = useRouter();
 
     const { currentTheme } = useContext(ThemeContext);
+    const { startTimer, stopTimer, resetTimer, elapsedTime } = useTimer();
     const isDark = currentTheme === "dark";
     const textClass = isDark ? "text-secondary" : "text-darkBg";
     const bgClass = isDark ? "bg-darkBg" : "bg-lightBg";
@@ -29,6 +31,23 @@ export default function Overlay({
             router.push(redirect);
         }
     }, [state, router]);
+
+    // Format elapsed time (mm:ss)
+    const formatTime = (seconds: number) => {
+        const mins = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${mins}:${secs.toString().padStart(2, "0")}`;
+    };
+
+    useEffect(() => {
+        if (state === Process.PENDING) {
+            resetTimer();
+            startTimer();
+        } else if (state === Process.READY) {
+            stopTimer();
+            if (redirect) router.push(redirect);
+        }
+    }, [state]);
 
     // Do not render overlay
     if (state != Process.PENDING) return null;
@@ -55,6 +74,9 @@ export default function Overlay({
                     {waitMsg || "Please wait for a moment..."}
                 </Text>
                 <ActivityIndicator size={60} color={"#006fff"} />
+                <Text className="font-poppinsRegular text-black">
+                    {formatTime(elapsedTime)}
+                </Text>
             </View>
         </View>
     );
