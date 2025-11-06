9783aab04ea3b76ba2656cff06513b3400c1f05a
GizakiF <aranetagian4221@gmail.com>
2025-11-06 19:30:28 +0800

    wrapped it with noisereduction context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,9 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Stack } from "expo-router";
 import {
-    StatusBar,
-    setStatusBarBackgroundColor,
-    setStatusBarTranslucent,
+	StatusBar,
+	setStatusBarBackgroundColor,
+	setStatusBarTranslucent,
 } from "expo-status-bar";
 import BottomNavigationSheet from "@/components/BottomNavigationSheet";
 import { BottomSheetProvider } from "../context/BottomSheetContext";
@@ -12,67 +12,68 @@ import ThemeProvider, { ThemeContext } from "@/context/ThemeContext";
 import { WienerProvider } from "@/context/WienerFilteringContext";
 import { TimerProvider } from "@/context/TimerContext";
 import { useContext, useEffect } from "react";
+import { NoiseReductionProvider } from '@/context/NoiseReductionContext';
 
 const { Screen } = Stack;
 
 // Root layout wrapped with ThemeProvider
 export default function RootLayout() {
-    return (
-        <TimerProvider>
-            <ThemeProvider>
-                <WienerProvider>
-                    <InnerLayout />
-                </WienerProvider>
-            </ThemeProvider>
-        </TimerProvider>
-    );
+	return (
+		<TimerProvider>
+			<ThemeProvider>
+				<NoiseReductionProvider>
+					<InnerLayout />
+				</NoiseReductionProvider>
+			</ThemeProvider>
+		</TimerProvider>
+	);
 }
 
 // Context must be used inside the provider
 function InnerLayout() {
-    const { currentTheme } = useContext(ThemeContext);
+	const { currentTheme } = useContext(ThemeContext);
 
-    useEffect(() => {
-        const bgColor = currentTheme === "dark" ? "#01000F" : "#FFFFFF";
+	useEffect(() => {
+		const bgColor = currentTheme === "dark" ? "#01000F" : "#FFFFFF";
 
-        setStatusBarTranslucent(false);
-        setStatusBarBackgroundColor(bgColor, true);
-    }, [currentTheme]);
+		setStatusBarTranslucent(false);
+		setStatusBarBackgroundColor(bgColor, true);
+	}, [currentTheme]);
 
-    return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <BottomSheetProvider>
-                <StatusBar
-                    translucent={false}
-                    style={currentTheme === "dark" ? "light" : "dark"}
-                    backgroundColor={
-                        currentTheme === "dark" ? "#01000F" : "#FFFFFF"
-                    }
-                />
-                <Stack
-                    screenOptions={{
-                        headerShown: false,
-                        animation: "none",
-                    }}
-                >
-                    <Screen name="index" options={{ title: "" }} />
-                    <Screen name="recording" options={{ title: "Recording" }} />
-                    <Screen
-                        name="select_language"
-                        options={{ title: "Select Language" }}
-                    />
-                    <Screen name="analysis" options={{ title: "Analysis" }} />
-                    <Screen
-                        name="classification"
-                        options={{ title: "Classification" }}
-                    />
-                    <Screen
-                        name="feature-analysis"
-                        options={{ title: "Feature Analysis" }}
-                    />
-                </Stack>
-                <BottomNavigationSheet />
-            </BottomSheetProvider>
-        </GestureHandlerRootView>
-    );
+	return (
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<BottomSheetProvider>
+				<StatusBar
+					translucent={false}
+					style={currentTheme === "dark" ? "light" : "dark"}
+					backgroundColor={
+						currentTheme === "dark" ? "#01000F" : "#FFFFFF"
+					}
+				/>
+				<Stack
+					screenOptions={{
+						headerShown: false,
+						animation: "none",
+					}}
+				>
+					<Screen name="index" options={{ title: "" }} />
+					<Screen name="recording" options={{ title: "Recording" }} />
+					<Screen
+						name="select_language"
+						options={{ title: "Select Language" }}
+					/>
+					<Screen name="analysis" options={{ title: "Analysis" }} />
+					<Screen
+						name="classification"
+						options={{ title: "Classification" }}
+					/>
+					<Screen
+						name="feature-analysis"
+						options={{ title: "Feature Analysis" }}
+					/>
+				</Stack>
+				<BottomNavigationSheet />
+			</BottomSheetProvider>
+		</GestureHandlerRootView>
+	);
 }
